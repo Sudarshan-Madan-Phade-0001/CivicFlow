49658da5de69be057078e6b0711dbaac01061ae1
Sudarshan Madan Phade <sudarshanphade0001@gmail.com>
2025-09-20 14:46:10 +0530

    Add gunicorn and production config for Render deployment

--- app.py
+++ app.py
@@ -634,4 +634,7 @@ def admin_setup():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+else:
+    init_db()
